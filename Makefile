3a67cf4ec433257f6758a53a5f8d288bb6f057a6
Dimka <22008248+Crickus@users.noreply.github.com>
2022-02-16 17:20:26 +0200

    Homework

--- Makefile
+++ Makefile
@@ -4,19 +4,21 @@ USERNAME := crickus
 # help:
 #         make -pRrq  -f $(THIS_FILE) : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$$'
 build-ui:
-	docker build -f ./docker/src/ui/Dockerfile -t $(USERNAME)/ui:latest
+	docker build -f ./docker/src/ui/Dockerfile -t $(USERNAME)/ui:latest .
 build-comment:
-	docker build -f ./docker/src/comment/Dockerfile -t $(USERNAME)/comment:latest
+	docker build -f ./docker/src/comment/Dockerfile -t $(USERNAME)/comment:latest .
 build-post:
-	docker build -f ./docker/src/post/Dockerfile -t $(USERNAME)/post:latest
+	docker build -f ./docker/src/post/Dockerfile -t $(USERNAME)/post:latest .
 build-prometheus:
-	docker build -f ./monitoring/prometheus/Dockerfile -t $(USERNAME)/prometheus:latest	
+	docker build -f ./monitoring/prometheus/Dockerfile -t $(USERNAME)/prometheus:latest	.
 build:
-	docker-compose -f ./docker/src/docker-compose.yml build $(c)
+	docker-compose -f ./docker/monitoring/compose/docker-compose.yml build $(c)
 push:
 	docker-compose -f ./docker/src/docker-compose.yml push $(c)
 up:
-	docker-compose -f ./docker/src/docker-compose.yml up -d $(c)
+	docker-compose -f ./docker/monitoring/compose/docker-compose.yml up -d $(c)
+up-mon:
+	docker-compose -f ./docker/monitoring/compose/docker-compose-monitoring.yml up -d $(c)
 start:
 	docker-compose -f ./docker/src/docker-compose.yml start $(c)
 down:
--- docker/docker_monolith/infra/terraform/modules/vpc/main.tf
+++ docker/docker_monolith/infra/terraform/modules/vpc/main.tf
@@ -94,3 +94,115 @@ resource "google_compute_firewall" "firewall_sw_overlay" {
   source_ranges = "${var.source_ranges}"
   target_tags   = ["docker"]
 }
+
+resource "google_compute_firewall" "firewall_cadvisor" {
+  name        = "default-allow-cadvisor"
+  network     = "default"
+  description = "Allow cAdvisor traffic from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_mongo_exporter" {
+  name        = "default-allow-mongo-exporter"
+  network     = "default"
+  description = "Allow mongodb exporter traffic from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9216"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_grafana" {
+  name        = "default-allow-grafana"
+  network     = "default"
+  description = "Allow grafana traffic from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_post_metrics" {
+  name        = "default-allow-post-metrics"
+  network     = "default"
+  description = "Allow post metrics gathering from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_alertmanager" {
+  name        = "default-allow-alertmanager"
+  network     = "default"
+  description = "Allow alertmanager posts from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9093"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_docker_monitor" {
+  name        = "default-allow-docker-monitor"
+  network     = "default"
+  description = "Allow docker monitor from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9323"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_telegraf_mon" {
+  name        = "default-allow-telegraf-mon"
+  network     = "default"
+  description = "Allow telegraf monitoring from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9273"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+# resource "google_compute_firewall" "firewall_stackdriver" {
+#   name        = "default-allow-stackdriver"
+#   network     = "default"
+#   description = "Allow stackdriver monitoring from anywhere"
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["9255"]
+#   }
+
+#   source_ranges = "${var.source_ranges}"
+#   target_tags   = ["docker"]
+# }
