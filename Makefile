83af22f7b6a907a1d7f562a82784d7da127bd7b3
Dimka <22008248+Crickus@users.noreply.github.com>
2022-02-16 11:30:13 +0200

    Homework update

---
+++ Makefile
@@ -0,0 +1,35 @@
+THIS_FILE := $(lastword $(MAKEFILE_LIST))
+USERNAME := crickus
+.PHONY: help build up start down destroy stop restart logs logs-api ps login-timescale login-api db-shell
+# help:
+#         make -pRrq  -f $(THIS_FILE) : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$$'
+build-ui:
+	docker build -f ./docker/src/ui/Dockerfile -t $(USERNAME)/ui:latest
+build-comment:
+	docker build -f ./docker/src/comment/Dockerfile -t $(USERNAME)/comment:latest
+build-post:
+	docker build -f ./docker/src/post/Dockerfile -t $(USERNAME)/post:latest
+build-prometheus:
+	docker build -f ./monitoring/prometheus/Dockerfile -t $(USERNAME)/prometheus:latest	
+build:
+	docker-compose -f ./docker/src/docker-compose.yml build $(c)
+push:
+	docker-compose -f ./docker/src/docker-compose.yml push $(c)
+up:
+	docker-compose -f ./docker/src/docker-compose.yml up -d $(c)
+start:
+	docker-compose -f ./docker/src/docker-compose.yml start $(c)
+down:
+	docker-compose -f ./docker/src/docker-compose.yml down $(c)
+destroy:
+	docker-compose -f ./docker/src/docker-compose.yml down -v $(c)
+stop:
+	docker-compose -f ./docker/src/docker-compose.yml stop $(c)
+restart:
+	docker-compose -f ./docker/src/docker-compose.yml stop $(c)
+	docker-compose -f ./docker/src/docker-compose.yml up -d $(c)
+logs:
+	docker-compose -f ./docker/src/docker-compose.yml logs --tail=100 -f $(c)
+ps:
+	docker-compose -f ./docker/src/docker-compose.yml ps
+
---
+++ docker/docker_monolith/infra/terraform/modules/vpc/main.tf
@@ -0,0 +1,96 @@
+resource "google_compute_firewall" "firewall_ssh" {
+  name        = "default-allow-ssh"
+  network     = "default"
+  description = "Allow SSH from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name        = "default-allow-puma"
+  network     = "default"
+  description = "Allow Puma from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_prometheus" {
+  name        = "default-allow-prometheus"
+  network     = "default"
+  description = "Allow Prometheus from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9090"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_sw_cluster" {
+  name        = "default-allow-sw-cluster"
+  network     = "default"
+  description = "Allow Swarm cluster conrtol from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["2377"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_sw_nodes_tcp" {
+  name        = "default-allow-sw-nodes-tcp"
+  network     = "default"
+  description = "Allow Swarm nodes TCP traffic from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["7946"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_sw_nodes_udp" {
+  name        = "default-allow-sw-nodes-udp"
+  network     = "default"
+  description = "Allow Swarm nodes UDP traffic from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["7946"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
+
+resource "google_compute_firewall" "firewall_sw_overlay" {
+  name        = "default-allow-sw-overlay"
+  network     = "default"
+  description = "Allow Swarm overlay traffic from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["4789"]
+  }
+
+  source_ranges = "${var.source_ranges}"
+  target_tags   = ["docker"]
+}
